1929b069376aa7e9c40ed63e03a8aee919116a0f
Joshua Taylor <toasterrage@gmail.com>
2023-03-05 15:49:42 -0700

    Fixed icon not showing

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,14 +34,16 @@ module.exports = () => {
         theme_color: '#7eb4e2',
         start_url: './',
         publicPath: './',
+        fingerprints: false,
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
+            fingerprints: false,
           },
         ],
-      }),
+      })
     ],
 
     module: {
