e9791c5cc1a867dee47be0bbc0fac508c9c7c798
Joshua Taylor <toasterrage@gmail.com>
2023-03-08 00:30:38 -0700

    Trying to get install working, might have run into linux issue

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,47 @@
 const butInstall = document.getElementById('buttonInstall');
 
+// USED https://web.dev/customize-install/ TO TRY TO GET THIS WORKING
+// NOT SURE IF THIS IS A LINUX-CHROME ISSUE
+// THE 'INSTALL' IS NOT SOMETHING I CAN FIND IN MY BROWSER
+// TODO - GET THIS WORKING
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  showInstallPromotion();
+
+  // Hide install button
+  butInstall.style.display = 'none';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+
+  hideInstallPromotion();
+  deferredPrompt.prompt();
+  const { outcome } = await deferredPrompt.userChoice;
+  deferredPrompt = null;
+
+  /*
+  const event = window.deferredPrompt;
+
+  if (!event) {
+    return;
+  }
+
+  event.prompt();
+  window.deferredPrompt = null;
+  // Toggle install button
+  //butInstall.style.display = 'initial';
+  */
+});
+
+// Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+  hideInstallPromotion();
+  deferredPrompt = null;
+
+});
